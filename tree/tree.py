e405e43572db1e3af8764403bf606a981368672f
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-09-09 11:49:33 +0100

    Update tree.py

--- tree/tree.py
+++ tree/tree.py
@@ -12,7 +12,7 @@ def tree_sort_by_extension(path=getcwd(), indent=0):
     print(indent*'  ' + '|-', path.split('/')[-1])
     for file in sorted(listdir(path), key=sort_key):
         if isdir(join(path, file)):
-            tree(join(path, file), indent+2)
+            tree_sort_by_extension(join(path, file), indent+2)
         else:
             print('  ' + indent*' ' + '|-', file)
 
@@ -20,7 +20,7 @@ def tree_sort_by_filename(path=getcwd(), indent=0):
     print(indent*'  ' + '|-', path.split('/')[-1])
     for file in sorted(listdir(path)):
         if isdir(join(path, file)):
-            tree(join(path, file), indent+2)
+            tree_sort_by_filename(join(path, file), indent+2)
         else:
             print('  ' + indent*' ' + '|-', file)
 
@@ -28,6 +28,6 @@ def tree_no_sorting(path=getcwd(), indent=0):
     print(indent*'  ' + '|-', path.split('/')[-1])
     for file in listdir(path):
         if isdir(join(path, file)):
-            tree(join(path, file), indent+2)
+            tree_no_sorting(join(path, file), indent+2)
         else:
             print('  ' + indent*' ' + '|-', file)
